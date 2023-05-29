b42b7b5ecee570a74926fddd77a6b880340896cf
Jingjing Li <jl3676@cornell.edu>
2023-05-29 09:25:24 -0700

    model validation for probrev

---
+++ Probabilistic_Reversal/code/recover_params.m
@@ -0,0 +1,109 @@
+% This script runs the "generate and recover" process for model parameters.
+% It generates simulated data using two predefined models, static and dynamic,
+% and then attempts to recover the model parameters using optimization methods.
+
+clear all;
+
+%% Define Models
+% Set options for the optimization process
+options = optimoptions('fmincon','Display','off');
+
+% Create a cell array to store the models
+Ms = [];
+
+% Define the static model and its parameter limits
+curr_model = [];
+curr_model.name = 'static_model';
+curr_model.pMin = [1e-6, -1, 1e-6];
+curr_model.pMax = [1, 1, 1];
+curr_model.pnames = {'alpha', 'stick', 'epsilon'};
+
+% Add the static model to the list of models
+Ms{1} = curr_model;
+
+% Define the dynamic model and its parameter limits
+curr_model = [];
+curr_model.name = 'dynamic_model';
+curr_model.pMin = [1e-6, -1, 1e-6, 1e-6];
+curr_model.pMax = [1, 1, 1, 1];
+curr_model.pnames = {'alpha', 'stick', 'lapse', 'recover'};
+
+% Add the dynamic model to the list of models
+Ms{2} = curr_model;
+
+%% Simulate Data with Model
+num_subjects = 500;
+pmin = Ms{2}.pMin;
+pmax = Ms{2}.pMax;
+names = Ms{2}.pnames;
+
+alpha_sample = @(x) betarnd(3, 10);
+stick_sample = @(x) normrnd(0, .1);
+lapse_sample = @(x) betarnd(1, 15);
+rec_sample = @(x) betarnd(1, 15);
+
+sampling_funcs = {alpha_sample, stick_sample, lapse_sample, rec_sample};
+
+% Iterate over subjects to generate and recover data
+genrec = cell(num_subjects, 1);
+
+parfor it = 1:num_subjects
+    % Simulate data
+    theta = zeros(length(pmin), 1);
+    for p = 1:length(pmin)
+        theta(p) = sampling_funcs{p}(0);
+    end
+    data = dynamic_model(theta);
+
+    % Find optimal parameters
+    par = pmin + rand(1, length(pmin)) .* (pmax - pmin);
+    myfitfun = @(p) dynamic_model_llh(p, data);
+
+    rng shuffle
+    fmincon_opts = optimoptions(@fmincon, 'Algorithm', 'sqp');
+    problem = createOptimProblem('fmincon', 'objective', ...
+        myfitfun, 'x0', par, 'lb', pmin, 'ub', pmax, 'options', fmincon_opts);
+    gs = GlobalSearch;
+    [param, llh] = run(gs, problem);
+
+    genrec{it} = [theta param'];
+end
+
+%% Plot Generate and Recover Results
+figure('Position', [300 300 600 600])
+
+for p = 1:length(names)
+    subplot(2, 2, p)
+    x1 = cell2mat(cellfun(@(x) x(p, 1), genrec, 'uni', 0));
+    x2 = cell2mat(cellfun(@(x) x(p, 2), genrec, 'uni', 0));
+    scatter(x1, x2, 'Marker', '.')
+    hold on
+    
+    if p == 3 || p ==  4
+        x1 = x1 + .001;
+        x2 = x2 + .001;
+        set(gca, 'XScale', 'log')
+        set(gca, 'YScale', 'log')
+        b = polyfit(log(x1), log(x2), 1);
+        fit = exp(b(2)) .* x1.^b(1);
+        plot(x1, fit, 'Color', [.75 .75 .75])
+    else
+        lsline
+    end
+    
+    title(names{p}, 'Interpreter', 'none')
+    xlabel('True')
+    ylabel('Recovered')
+    hold on
+    plot(ylim, ylim, 'r')
+    xlim([min(x1) max(x1)])
+    ylim([min(x2) max(x2)])
+end
+
+% Set the title of the overall figure
+h = sgtitle('Dynamic Model');
+h.Interpreter = 'none';
+
+% Save the figure as PNG and SVG files
+saveas(gcf, '../plots/genrec_dynamic.png')
+saveas(gcf, '../plots/genrec_dynamic.svg')
---
+++ Probabilistic_Reversal/code/validate_models.m
@@ -0,0 +1,248 @@
+% This script performs simulations, fits models to the simulated data,
+% and plots the validation results based on the latent state.
+
+clear all;
+
+%% Set Up
+% Define sampling functions for parameter generation
+alpha_sample = @(x) betarnd(3, 10);
+stick_sample = @(x) normrnd(0, .1);
+lapse_sample = @(x) betarnd(1, 15);
+rec_sample = @(x) betarnd(1, 15);
+
+% Store the sampling functions in a cell array
+sampling_funcs = {alpha_sample, stick_sample, lapse_sample, rec_sample};
+
+%% Simulate Participants
+num_subjects = 1000;
+Alldata = cell(num_subjects,1);
+Alllatent = cell(num_subjects,1);
+All_latent_st_traj = cell(num_subjects,1);
+sim_params = zeros(num_subjects, 4);
+
+% Iterate over participants to simulate data
+for i = 1:num_subjects
+    % Generate random parameters for each participant
+    theta = zeros(length(sampling_funcs),1);
+    for p = 1:length(theta)
+        theta(p) = sampling_funcs{p}(0);
+    end
+
+    sim_params(i,:) = theta;
+
+    % Simulate data for this participant
+    this_data = dynamic_model(theta);
+    this_latent = [this_data(:,1) this_data(:,end)];
+    Alldata{i} = this_data;
+    Alllatent{i} = this_latent;
+end
+
+%% Fit Models
+options = optimoptions('fmincon','Display','off');
+
+% Create a cell array to store the models
+Ms = [];
+
+% Define the static model and its parameter limits
+curr_model = [];
+curr_model.name = 'static_model';
+curr_model.pMin = [1e-6, -1, 1e-6];
+curr_model.pMax = [1, 1, 1];
+curr_model.pnames = {'alpha', 'stick', 'epsilon'};
+
+% Add the static model to the list of models
+Ms{1} = curr_model;
+
+% Define the dynamic model and its parameter limits
+curr_model = [];
+curr_model.name = 'dynamic_model';
+curr_model.pMin = [1e-6, -1, 1e-6, 1e-6];
+curr_model.pMax = [1, 1, 1, 1];
+curr_model.pnames = {'alpha', 'stick', 'lapse', 'recover'};
+
+% Add the dynamic model to the list of models
+Ms{2} = curr_model;
+
+All_Params = cell(length(Ms),1);
+All_fits = cell(length(Ms),1);
+
+% Fit models for each participant
+for m = 1:length(Ms)
+    fit_model = Ms{m};
+    pmin = fit_model.pMin;
+    pmax = fit_model.pMax;
+
+    fitmeasures = cell(num_subjects,1);
+    fitparams = cell(num_subjects,1);
+
+    parfor s = 1:num_subjects
+        data = Alldata{s};
+        par = pmin + rand().*(pmax-pmin);
+
+        myfitfun = @(p) feval([fit_model.name,'_llh'],p,data);
+        rng default % For reproducibility
+        opts = optimoptions(@fmincon,'Algorithm','sqp');
+        problem = createOptimProblem('fmincon','objective',...
+            myfitfun,'x0',par,'lb',pmin,'ub',pmax,'options',opts);
+        gs = GlobalSearch;
+        [param,llh] = run(gs,problem);
+
+        ntrials = size(data,1);
+
+        % Calculate fit measures
+        AIC = 2*llh + 2*length(param);
+        BIC = 2*llh + log(ntrials)*length(param);
+        AIC0 = -2*log(1/3)*ntrials;
+        psr2 = (AIC0-AIC)/AIC0;
+
+        fitmeasures{s} = [s llh AIC BIC psr2 AIC0];
+        fitparams{s} = param;
+    end
+
+    All_Params{m} = cell2mat(fitparams);
+    All_fits{m} = cell2mat(fitmeasures);
+end
+
+temp = All_fits;
+All_fits = zeros(num_subjects,size(temp{1},2),length(Ms));
+for i = 1:length(Ms)
+    All_fits(:,:,i) = temp{i};
+end
+
+%% Plot Validation Around Switches
+niters = 10;
+
+sim_data = zeros(num_subjects, 50, 2);
+static_data = zeros(num_subjects, 50, 2);
+dynamic_data = zeros(num_subjects, 50, 2);
+
+sim_choice_prob = zeros(num_subjects, 500);
+static_choice_prob = zeros(num_subjects, 500);
+dynamic_choice_prob = zeros(num_subjects, 500);
+
+latent_mask = zeros(num_subjects, 500);
+
+% Iterate over participants to generate simulation data and calculate choice probabilities
+for this_ID = 1:num_subjects
+    this_static_sim_engaged = zeros(500,6);
+    this_static_sim_random = zeros(500,6);
+    this_dynamic_sim_engaged = zeros(500,6);
+    this_dynamic_sim_random = zeros(500,6);
+    this_data_all_engaged = zeros(500,6);
+    this_data_all_random = zeros(500,6);
+
+    this_data = Alldata{this_ID};
+    this_latent = Alllatent{this_ID}(:,1);
+
+    latent_mask(this_ID,:) = this_latent;
+    sim_choice_prob(this_ID,:) = Alllatent{this_ID}(:,2);
+
+    for it = 1:niters
+        % Simulate data using static model
+        data_temp = static_model(All_Params{1}(this_ID,:));
+        latent = [data_temp(:,1) data_temp(:,end)];
+        this_static_sim_engaged(this_latent==1,:) = this_static_sim_engaged(this_latent==1,:) + data_temp(this_latent==1,:);
+        this_static_sim_random(this_latent==0,:) = this_static_sim_random(this_latent==0,:) + data_temp(this_latent==0,:);
+
+        static_choice_prob(this_ID,:) = static_choice_prob(this_ID,:) + latent(:,2)';
+
+        % Simulate data using dynamic model
+        latent_st_traj = zeros(1,500);
+        for i = 1:20
+            latent = dynamic_model_latent(All_Params{2}(this_ID,:), this_data);
+            latent_st_traj = latent_st_traj + latent(:,1);
+        end
+        latent_st_traj = latent_st_traj / 20;
+        data_temp = dynamic_model(All_Params{2}(this_ID,:), latent_st_traj);
+        latent = [data_temp(:,1) data_temp(:,end)];
+        this_dynamic_sim_engaged(this_latent==1,:) = this_dynamic_sim_engaged(this_latent==1,:) + data_temp(this_latent==1,:);
+        this_dynamic_sim_random(this_latent==0,:) = this_dynamic_sim_random(this_latent==0,:) + data_temp(this_latent==0,:);
+
+        dynamic_choice_prob(this_ID,:) = dynamic_choice_prob(this_ID,:) + latent(:,2)';
+    end
+
+    this_data_all_engaged(this_latent==1,:) = this_data_all_engaged(this_latent==1,:) + this_data(this_latent==1,:);
+    this_data_all_random(this_latent==0,:) = this_data_all_random(this_latent==0,:) + this_data(this_latent==0,:);
+
+    this_data_all_engaged(this_latent==0,4) = nan;
+    this_data_all_random(this_latent==1,4) = nan;
+    this_static_sim_engaged(this_latent==0,4) = nan;
+    this_static_sim_random(this_latent==1,4) = nan;
+    this_dynamic_sim_engaged(this_latent==0,4) = nan;
+    this_dynamic_sim_random(this_latent==1,4) = nan;
+
+    rewards_engaged = reshape(this_data_all_engaged(:,4), [50,10]);
+    rewards_engaged = nanmean(rewards_engaged, 2);
+    rewards_random = reshape(this_data_all_random(:,4), [50,10]);
+    rewards_random = nanmean(rewards_random, 2);
+
+    rewards_static_engaged = reshape(this_static_sim_engaged(:,4), [50,10]);
+    rewards_static_engaged = nanmean(rewards_static_engaged,2);
+    rewards_static_random = reshape(this_static_sim_random(:,4), [50,10]);
+    rewards_static_random = nanmean(rewards_static_random,2);
+
+    rewards_dynamic_engaged = reshape(this_dynamic_sim_engaged(:,4), [50,10]);
+    rewards_dynamic_engaged = nanmean(rewards_dynamic_engaged,2);
+    rewards_dynamic_random = reshape(this_dynamic_sim_random(:,4), [50,10]);
+    rewards_dynamic_random = nanmean(rewards_dynamic_random,2);
+
+    sim_data(this_ID,:,1) = rewards_engaged;
+    sim_data(this_ID,:,2) = rewards_random;
+    static_data(this_ID,:,1) = rewards_static_engaged / niters;
+    static_data(this_ID,:,2) = rewards_static_random / niters;
+    dynamic_data(this_ID,:,1) = rewards_dynamic_engaged / niters;
+    dynamic_data(this_ID,:,2) = rewards_dynamic_random / niters;
+end
+
+static_choice_prob = static_choice_prob / niters;
+dynamic_choice_prob = dynamic_choice_prob / niters;
+
+%% Plot
+window = 1;
+trials_to_plot = 15;
+
+xs = -trials_to_plot:3:trials_to_plot;
+figure('Position',[300,300,600,300])
+
+data_to_plot = {static_data, dynamic_data};
+
+for i = 1:2
+    subplot(1,2,i);
+    rewards_engaged = nanmean(sim_data(:,:,1),1)';
+    rewards_random = nanmean(sim_data(:,:,2),1)';
+
+    this_data = data_to_plot{i};
+
+    rewards_static_engaged = nanmean(this_data(:,:,1),1)';
+    rewards_static_random = nanmean(this_data(:,:,2),1)';
+
+    rewards_engaged = movmean(rewards_engaged,window);
+    rewards_random = movmean(rewards_random,window);
+    rewards_static_engaged = movmean(rewards_static_engaged,window);
+    rewards_static_random = movmean(rewards_static_random,window);
+
+    rewards_engaged = [rewards_engaged(end-trials_to_plot:end); rewards_engaged(1:trials_to_plot)];
+    rewards_random = [rewards_random(end-trials_to_plot:end); rewards_random(1:trials_to_plot)];
+    rewards_static_engaged = [rewards_static_engaged(end-trials_to_plot:end); rewards_static_engaged(1:trials_to_plot)];
+    rewards_static_random = [rewards_static_random(end-trials_to_plot:end); rewards_static_random(1:trials_to_plot)];
+
+    plot([trials_to_plot+1 trials_to_plot+1], [0 1], '--', 'Color',[.5 .5 .5])
+    hold on
+    plot(rewards_engaged,'k','LineWidth',2)
+    hold on
+    plot(rewards_random,'k--','LineWidth',2)
+    hold on
+    plot(rewards_static_engaged,'r','LineWidth',2)
+    hold on
+    plot(rewards_static_random,'r--','LineWidth',2)
+
+    xticks(1:3:2*trials_to_plot+1)
+    xticklabels(xs)
+    xlabel('Trial from switch')
+    ylabel('Accuracy')
+
+    legend({'', 'data engaged', 'data random', 'model engaged', 'model random'}, 'Interpreter','none','Location','best')
+end
+
+saveas(gcf,'../plots/validation_by_latent_st.png')
+saveas(gcf,'../plots/validation_by_latent_st.svg')
